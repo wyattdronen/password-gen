74b557c01475f6a5fb6b78d6f272f01544112cac
wyattdronen <137117182+wyattdronen@users.noreply.github.com>
2023-07-05 13:12:14 -0500

    final code

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # password-gen
+
+In this module i learned to use functions, sets, arrays, and the random number generator better. My tutor Rubin helped me to understand how to use answers from prompts as well as character types, and character generating as well as writing down comments to remember what part in the code/ function i am in.
--- script.js
+++ script.js
@@ -1,53 +1,53 @@
 
-
 // ask about how many characters
 
-// what type of data type is good for special characters?
-
-alert ("How many characters?")
-
-function getRandomNumber() {
-  var length = 8,
-      charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
-      retVal = "";
-  for (var i = 0, n = charset.length; i < length; ++i) {
-      retVal += charset.charAt(Math.floor(Math.random() * n));
-  }
-  return retVal;
-}
-
-
-// Assignment Code
+// Get references to the generate element
 var generateBtn = document.querySelector("#generate");
-function generatePassword(){
-//code here
 
-  var x = getRandomNumber(0,100);
-
-
-return
-}
-
-function askHowManyCharacters(){
-
-
-  askAboutSpecialCharacters()
-}
-
-
-function askAboutSpecialCharacters(){
-
-}
-
-
-// Write password to the #password input
+// Write password to the password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let passwordText = document.querySelector("#password");
+  
+  //Length
+  let length = Number(prompt("password length, 8-128 characters."))
+  //character types
+  let charType = prompt("What to include in the password? Type 'uppercase, lowercase, numbers, symbols' to include. ")
+  
+  passwordText.value = password(length, charType);
 
-  passwordText.value = password;
+  function password(length, charType) {
+    let charGen = {
+      lowercase: 'abcdefghijklmnop',
+      uppercase: 'ABCDEFGHIJKLMNOP',
+      numbers: '0123456789',
+      symbols: '!@#$%^&*()-_=+',
+    };
+    
+    var charTypes = charType.toLowerCase().split(', ');
+    charSet = "";
+    for(var i=0; i < charTypes.length; i++) {
+      charSet += charGen[charTypes[i]];
+    }
+    
+    console.log(charSet);
 
+    var retVal = "";
+    
+    for (var i = 0; i < length; i++) {
+      retVal += charSet.charAt(Math.floor(Math.random() * charSet.length));
+    }
+    return retVal;
+  }
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function copyPassword() {
+  document.getElementById("password").select();
+  navigator.clipboard.writeText(copyText.value);
+  alert("Password copied to clipboard!" + copyText.value);
+}
+
+
+// what type of data type is good for special characters?
